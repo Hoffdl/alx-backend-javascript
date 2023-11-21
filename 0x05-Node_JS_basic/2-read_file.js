8d467d311dee190d7fc39b612cfbadf3bf43c55d
Henry <ikegwuonuhenry25@gmail.com>
2023-11-21 16:44:16 +0000

    nodejs

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -16,8 +16,8 @@ function countStudents(fileName) {
         } else {
           students[field[3]] = [field[0]];
         }
-        if (Object.prototype.hasOwnProperty.call(fields, fields[3])) {
-          firlds[field[3]] += 1;
+        if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+          fields[field[3]] += 1;
 	} else {
 	  fields[field[3]] = 1;
 	}
